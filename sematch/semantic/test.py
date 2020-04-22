0f43777358e6e78ceb3b6d7813db30c1873d2b87
Daniel Stefanik <danielstefanik36@gmail.com>
2020-04-22 14:41:32 +0200

    update test script

--- sematch/semantic/test.py
+++ sematch/semantic/test.py
@@ -2,18 +2,19 @@ from sematch.semantic.similarity import WordNetSimilarity
 
 wn_sim = WordNetSimilarity()
 
-w1 = 'pies'
+w1 = 'gil'
 lang1 = 'pol'
-w2 = 'kot'
+w2 = "sowa"
 lang2 = 'pol'
 result = []
-sim_type = 'zhou'
+# for sim_type in ['path','lch','wup','li','res','lin','jcn','wpath','zhou']:
 
-tmp = {}
-tmp['name'] = sim_type
-tmp['sim'] = wn_sim.crossl_word_similarity(w1, w2, lang1, lang2, sim_type)
-result.append(tmp)
+for sim_type in ['path','wup','li','res','lin','jcn','wpath','zhou']:
+    sim = wn_sim.crossl_word_similarity(w1, w2, lang1, lang2, sim_type)
+    tmp = {'name': sim_type, 'sim': sim}
+    result.append(tmp)
+    print(tmp)
 
-print(tmp)
-
+avg = (result[0]['sim'] + result[1]['sim'] + result[2]['sim'] + result[3]['sim']/10 + result[4]['sim'] + result[5]['sim'] + result[6]['sim'])/7
+print("average from other methods: "+str(avg))
 
